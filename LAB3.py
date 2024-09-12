63f6382700d955a388d701114fc2a15b3370cb06
jillkaria2709 <157040744+jillkaria2709@users.noreply.github.com>
2024-09-12 14:27:38 +0000

    3a

---
+++ LAB3.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import openai as OpenAI
+
+st.title('My LAB3 Question Answering chatbox')
+
+openAImodel = st.sidebar.selectbox("Which model?", ("mini", "regular"))
+
+if openAImodel=="mini":
+    model_to_use = "gpt-4o-mini"
+else:
+    model_to_use = "gpt-4o"
+
+if 'client' not in st.session_state:
+    api_key = st.secrets["openai_key"]
+    st.session_state.client = OpenAI(api_key=api_key)
+
+if 'messages' not in st.session_state:
+    st.session_state["messages"] = \
+        [{"role":"assistant","content": "How can I help you?"}]
+    
+for msg in st.session_state.messages:
+    chat_msg = st.chat_message(msg["role"])
+    chat_msg.write(msg["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    client = st.session_state.client
+    stream = client.chat.completions.create(
+        model = model_to_use,
+        messages = st.session_state.messages,
+        stream = True
+    )
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream)
+
+    st.session_state.messages.append({"role":"assisnant", "content":response})
+    
