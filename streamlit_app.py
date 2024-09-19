820dabec2af0fe1329a1e2f68685bef87bd441d3
jillkaria2709 <157040744+jillkaria2709@users.noreply.github.com>
2024-09-19 14:42:06 +0000

    dadada

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,13 @@ import streamlit as st
 from streamlit import session_state as state
 
 # Define individual pages for homework 1 and homework 2
+lab4_page = st.Page("LAB4.py", title="Lab 4")
 lab3_page = st.Page("LAB3.py", title="Lab 3")
 lab2_page = st.Page("LAB2.py", title="Lab 2")
 lab1_page = st.Page("LAB1.py", title="Lab 1")
 
 # Initialize navigation with the pages
-pg = st.navigation([lab3_page,lab2_page, lab1_page])
+pg = st.navigation([lab4_page,lab3_page,lab2_page, lab1_page])
 
 # Set page configuration (optional but helps with page title and icon)
 st.set_page_config(page_title="Lab Manager", page_icon=":memo:")
